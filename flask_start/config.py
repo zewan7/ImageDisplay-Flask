9c30931db17858ddb319156a5ab4e887527c1494
zewan7 <67500746+zewan7@users.noreply.github.com>
2025-02-25 10:46:36 +0800

    Update config.py

--- flask_start/config.py
+++ flask_start/config.py
@@ -4,10 +4,10 @@ import os
 # 然后使用SQLAlchemy(APP）创建一个APP对象
 # 会自动读取config链接信息
 # 阿里云服务器上mysql
-HOSTNAME = "47.109.52.163"
+HOSTNAME = "47."
 POST = "3306"
-USERNAME = "ee"
-PASSEORD = "5tgb%TGB"
+USERNAME = "e"
+PASSEORD = "5"
 DATABASE = "web202211"
 DB_URI = f"mysql+pymysql://{USERNAME}:{PASSEORD}@{HOSTNAME}:{POST}/{DATABASE}?charset=utf8mb4"
 # DB_URI = f"mysql+pymysql://{USERNAME}:{PASSEORD}@{HOSTNAME}:{POST}/{DATABASE}"
@@ -17,8 +17,8 @@ SQLALCHEMY_DATABASE_URI = DB_URI
 MAIL_SERVER = "smtp.qq.com"
 MAIL_USE_SSL = True
 MAIL_PORT = 465
-MAIL_USERNAME = "cukuangren@qq.com"
-MAIL_PASSWORD = "wcwucanuuavacbbf"
+MAIL_USERNAME = "cukua"
+MAIL_PASSWORD = "wcw***********"
 MAIL_DEFAULT_SENDER = "cukuangren@qq.com"
 
 SECRET_KEY = os.urandom(18)
